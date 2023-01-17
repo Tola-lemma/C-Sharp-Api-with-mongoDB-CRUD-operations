20102a6eb52217d689fa087b76f2f6687853b750
Tola-lemma <tolalemma@gmail.com>
2023-01-17 14:40:44 -0800

    final commit

--- Program.cs
+++ Program.cs
@@ -3,9 +3,7 @@ using projectOneWithMongo.Models;
 using projectOneWithMongo.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
